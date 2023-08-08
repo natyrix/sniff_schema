b3245ab6b794f39a9e13b328dde57d6934c1be9f
Natnael Melese <natrix277@gmail.com>
2023-08-08 06:33:57 +0300

    slight modification

--- scripts/sniffer.py
+++ scripts/sniffer.py
@@ -5,10 +5,13 @@ import os
 class Sniffer:
 
     def __init__(self) -> None:
+        # Output directory where schema files will be created
         self.output_dir = "./schema"
+        # Directory where input JSON files are located
         self.data_dir = "./data"
 
     def sniff_schema(self, json_input: dict) -> dict:
+        # Generate schema dictionary based on input JSON
         schema = {}
         message = json_input.get("message", None)
 
@@ -16,9 +19,9 @@ class Sniffer:
             return schema
 
         for key, value in message.items():
-            if isinstance(value, dict):
-                pass
-            elif isinstance(value, list):
+            if isinstance(value, dict):# If value is a dictionary
+                pass  # Schema handling for nested dictionaries is not required in problem description
+            elif isinstance(value, list):# If value is a list
                 if len(value) > 0:
                     schema_type = "enum" if isinstance(value[0], str) else "array"
                     schema[key] = {
@@ -27,14 +30,14 @@ class Sniffer:
                         "description": "",
                         "required": False
                     }
-            elif isinstance(value, str):
+            elif isinstance(value, str): # If value is a string
                 schema[key] = {
                     "type": "string",
                     "tag": "",
                     "description": "",
                     "required": False
                 }
-            elif isinstance(value, int):
+            elif isinstance(value, int): # If value is an integer
                 schema[key] = {
                     "type": "integer",
                     "tag": "",
@@ -44,6 +47,7 @@ class Sniffer:
         return schema
 
     def create_schema_file(self, input_json: dict, out_put_path: str, file_name: str) -> None:
+        # Create a schema file from a given JSON dictionary
         with open(f"{out_put_path}/{file_name}", "w") as f:
             json.dump(input_json, f, indent=4)
 
@@ -52,7 +56,7 @@ class Sniffer:
     def run(self):
         try:
             for i, filename in enumerate(os.listdir(self.data_dir)):
-                if filename.endswith(".json"):
+                if filename.endswith(".json"): # If the file is a JSON file
                     file_path = os.path.join("./data", filename)
                     with open(file_path, "r") as file:
                         json_data = json.load(file)
